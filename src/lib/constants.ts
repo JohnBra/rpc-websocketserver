af1ff0e3f9237d7cd08aff62db313f9d62074aa6
Jonathan Braat <jonathan.braat@live.com>
2020-07-26 15:19:44 +0200

    [ADD] ignore NOOP in code coverage

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 import { Method, Params } from './message-handler';
 
+/* istanbul ignore next */
 export const NOOP = (): void => {};
+
 export const EMPTY_PARAMS: Params = {};
 export const EMPTY_METHOD: Method = { namespace: '', name: '', params: EMPTY_PARAMS, func: NOOP };
