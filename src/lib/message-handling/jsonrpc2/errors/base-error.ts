74bb9cc73c4540488e74695c367d704c15235045
John <jonathan.braat@live.com>
2020-08-02 13:35:02 +0200

    [MOD] make json rpc 2 base error abstract

--- src/lib/message-handling/jsonrpc2/errors/base-error.ts
+++ src/lib/message-handling/jsonrpc2/errors/base-error.ts
@@ -1,11 +1,11 @@
 import { JSONRPC2Error, JSONRPC2ErrorDetails } from '../utils';
 
-export class JSONRPC2BaseError extends Error {
+export abstract class JSONRPC2BaseError extends Error {
     public code: number;
     public message: string;
     public details: JSONRPC2ErrorDetails | undefined;
 
-    constructor(details?: JSONRPC2ErrorDetails) {
+    protected constructor(details?: JSONRPC2ErrorDetails) {
         super();
         this.name = 'BaseError';
         this.code = -32099;
