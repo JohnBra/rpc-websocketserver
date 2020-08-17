17041f30e81dd568028bb1eb1dee938f2cb4618f
John <jonathan.braat@live.com>
2020-08-17 18:08:10 +0200

    [ADD] tests

---
+++ src/__tests__/message-handlers/simple.spec.ts
@@ -0,0 +1,48 @@
+import Simple from '../../lib/message-handlers/simple';
+import { HandlerResult, Method } from '../../lib/interfaces';
+import { NOOP } from '../../lib/constants';
+
+class MockClass {
+    getThrowErrorFunction(): Function {
+        return this.throwError;
+    }
+
+    getMockSumFunction(): Function {
+        return this.mockSum;
+    }
+
+    mockSum(a: string, b: number): string {
+        return a + b;
+    }
+
+    throwError(): void {
+        throw Error('Some error');
+    }
+}
+
+describe('SimpleMessageHandler class', () => {
+    let registeredMethodA: Method;
+    let registeredMethods: Map<string, Method>;
+    let registeredMethodAExpectedArgs: Array<any>;
+
+    beforeAll(()=> {
+        registeredMethodA = {
+            namespace: 'MockNamespace',
+            name: 'mockMethodA',
+            params: { a: 'string', b: 'number' },
+            func: () => {}
+        };
+        registeredMethodAExpectedArgs = ['abc', 1];
+        registeredMethods = new Map<string, Method>();
+        registeredMethods.set(registeredMethodA.name, registeredMethodA);
+    });
+
+    test('constructor call without parameters does not throw error', () => {
+
+        function instantiateSimpleMessageHandler() {
+            new Simple();
+        }
+
+        expect(instantiateSimpleMessageHandler).not.toThrow();
+    });
+});
--- src/__tests__/websocket-server.spec.ts
+++ src/__tests__/websocket-server.spec.ts
@@ -1,30 +1,72 @@
-import * as WebSocket from 'ws';
+import WS from 'jest-websocket-mock';
+import * as WebSocket from 'ws'; // only used for ws typings
 import { WebSocketServer } from '../lib/websocket-server';
-import { MessageHandler } from '../lib/interfaces';
-import Simple from '../lib/message-handlers/simple';
+import { HandlerResult, MessageHandler, Method } from '../lib/interfaces';
+import { NOOP } from '../lib/constants';
+import { register } from '../lib/decorators';
+
+class MockMessageHandler implements MessageHandler {
+    handle(message: WebSocket.Data, registeredMethods: Map<string, Method>): HandlerResult {
+        return { error: false, data: undefined, func: NOOP, args: [] };
+    }
+
+    process(handlerResult: HandlerResult): WebSocket.Data | Promise<WebSocket.Data | undefined> | undefined {
+        return undefined;
+    }
+
+}
 
 
-
-class MockNamespace extends WebSocketServer {
+class MockNamespaceA extends WebSocketServer {
     constructor(messageHandler: MessageHandler, options: WebSocket.ServerOptions) {
         super(messageHandler, options);
     }
 }
 
+class MockNamespaceB extends WebSocketServer {
+    constructor(messageHandler: MessageHandler, options: WebSocket.ServerOptions) {
+        super(messageHandler, options);
+    }
+
+    @register()
+    sum(a: number, b: number): number {
+        return a + b;
+    }
+}
+
 describe('WebSocketServer abstract class', () => {
     let wssOptions: WebSocket.ServerOptions;
-    let simpleMessageHandler: Simple;
+    let mockMessageHandler: MessageHandler;
 
     beforeAll(() => {
         wssOptions = { noServer: true };
-        simpleMessageHandler = new Simple();
+        mockMessageHandler = new MockMessageHandler();
     });
 
-    test('instantiates without failure', () => {
+
+
+    it('should instantiate without failure', () => {
         function instantiateMockNamespace() {
-            new MockNamespace(simpleMessageHandler, wssOptions);
+            new MockNamespaceA(mockMessageHandler, wssOptions);
         }
 
         expect(instantiateMockNamespace).not.toThrow(Error);
+    });
+
+    it('getMethods() should return Map (string, Method) of namespace methods', () => {
+        const mockNamespaceA = new MockNamespaceA(mockMessageHandler, wssOptions);
+        const methodsNamespaceA = mockNamespaceA.getMethods();
+
+        const mockNamespaceB = new MockNamespaceB(mockMessageHandler, wssOptions);
+        const methodsNamespaceB = mockNamespaceB.getMethods();
+
+        expect(methodsNamespaceA.size).toBe(0);
+        expect(methodsNamespaceA.get('sum')).toBeUndefined();
+        expect(methodsNamespaceB.size).toBe(1);
+        expect(methodsNamespaceB.get('sum')).not.toBeUndefined();
+    });
+
+    it('broadcastMessage(data) should send a message to all connected clients', () => {
+
     });
 });
