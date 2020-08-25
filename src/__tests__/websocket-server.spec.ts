48cf36ab3533cfc0376aba17883a106a762b8a2a
John <jonathan.braat@live.com>
2020-08-25 20:54:59 +0200

    [ADD] websocket server tests

--- src/__tests__/websocket-server.spec.ts
+++ src/__tests__/websocket-server.spec.ts
@@ -1,9 +1,95 @@
-import WS from 'jest-websocket-mock';
-import * as WebSocket from 'ws'; // only used for ws typings
+import WebSocket from 'ws';
 import { WebSocketServer } from '../lib/websocket-server';
 import { HandlerResult, MessageHandler, Method } from '../lib/interfaces';
 import { NOOP } from '../lib/constants';
 import { register } from '../lib/decorators';
+
+import express from 'express';
+import http from 'http';
+import url from 'url';
+
+
+class MockClient {
+    ws: WebSocket;
+    messages: Array<any>;
+
+    constructor(host: string, port: number) {
+        this.ws = new WebSocket(`ws://${host}:${port}/`);
+        this.messages = [];
+    }
+
+    async open(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const timeout = setTimeout(() => reject(), 10000);
+            this.ws.on('open', () => {
+                clearTimeout(timeout);
+                resolve();
+            });
+        });
+    }
+
+    async message(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const timeout = setTimeout(() => reject(), 10000);
+            this.ws.on('message', (data: any) => {
+                this.messages.push(data);
+                clearTimeout(timeout);
+                resolve();
+            });
+        });
+    }
+
+    clean() {
+        this.messages = [];
+        this.ws.close();
+    }
+}
+
+class MockServer {
+    app: any;
+    server: any;
+    host: string;
+    port: number;
+    mockNamespace: WebSocketServer;
+
+    constructor(host: string, port: number, mockNamespace: WebSocketServer) {
+        this.host = host;
+        this.port = port;
+        this.app = express();
+        this.server = http.createServer(this.app);
+        this.mockNamespace = mockNamespace;
+
+        this.server.on('upgrade', async (request: any, socket: any, head: any) => {
+            const { pathname } = url.parse(request.url);
+            if (pathname === '/') {
+                await this.handleUpgrade(request, socket, head);
+            } else {
+                socket.destroy();
+            }
+        });
+    }
+
+    async handleUpgrade(request: any, socket: any, head: any): Promise<void> {
+        return new Promise<void>(resolve => {
+            this.mockNamespace.wss.handleUpgrade(request, socket, head, (ws: WebSocket) => {
+                this.mockNamespace.wss.emit('connection', ws, request);
+                resolve();
+            });
+        });
+    }
+
+    async start(): Promise<void> {
+        return new Promise<void>(resolve => {
+            this.server.listen(this.port, this.host, 1024, () => { resolve(); });
+        });
+    }
+
+    clean() {
+        this.mockNamespace.wss.clients.forEach((ws: WebSocket) => {
+            ws.close();
+        });
+    }
+}
 
 class MockMessageHandler implements MessageHandler {
     handle(message: WebSocket.Data, registeredMethods: Map<string, Method>): HandlerResult {
@@ -13,9 +99,7 @@ class MockMessageHandler implements MessageHandler {
     process(handlerResult: HandlerResult): WebSocket.Data | Promise<WebSocket.Data | undefined> | undefined {
         return undefined;
     }
-
 }
-
 
 class MockNamespaceA extends WebSocketServer {
     constructor(messageHandler: MessageHandler, options: WebSocket.ServerOptions) {
@@ -29,26 +113,29 @@ class MockNamespaceB extends WebSocketServer {
     }
 
     @register()
-    sum(a: number, b: number): number {
+    foo(a: number, b: number): number {
         return a + b;
     }
 }
 
 describe('WebSocketServer abstract class', () => {
+    const host = 'localhost';
+    const port = 9999;
     let wssOptions: WebSocket.ServerOptions;
     let mockMessageHandler: MessageHandler;
     let mockNamespaceA: WebSocketServer;
     let mockNamespaceB: WebSocketServer;
+    let mockServer: MockServer;
 
-    beforeAll(() => {
+    beforeAll(async () => {
         wssOptions = { noServer: true };
         mockMessageHandler = new MockMessageHandler();
+        mockServer = new MockServer(host, port, new MockNamespaceB(mockMessageHandler, wssOptions));
+        try { await mockServer.start(); } catch(err) {}
     });
 
-    afterAll(() => {
-        mockNamespaceA.wss.clients.forEach((ws) => {
-            ws.close(0);
-        });
+    afterAll(async () => {
+        mockServer.clean();
     });
 
     it('should instantiate without failure', () => {
@@ -67,12 +154,19 @@ describe('WebSocketServer abstract class', () => {
         const methodsNamespaceB = mockNamespaceB.getMethods();
 
         expect(methodsNamespaceA.size).toBe(0);
-        expect(methodsNamespaceA.get('sum')).toBeUndefined();
+        expect(methodsNamespaceA.get('foo')).toBeUndefined();
         expect(methodsNamespaceB.size).toBe(1);
-        expect(methodsNamespaceB.get('sum')).not.toBeUndefined();
+        expect(methodsNamespaceB.get('foo')).not.toBeUndefined();
     });
 
-    it('broadcastMessage(data) should send a message to all connected clients', () => {
-        expect(1).toBe(1);
+    it('broadcastMessage(data) should send a message to all connected clients', async () => {
+        const mockClient = new MockClient(host, port);
+        await mockClient.open();
+
+        const message = 'a';
+        mockServer.mockNamespace.broadcastMessage(message);
+        await mockClient.message();
+        expect(mockClient.messages[0]).toBe(message);
+        mockClient.clean();
     });
 });
