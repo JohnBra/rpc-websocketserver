059950e44634f244978dc8256b542b729eff3f6d
John <jonathan.braat@live.com>
2020-08-30 00:15:39 +0200

    [ADD] interfaces comments

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -8,29 +8,33 @@ export type Params = Record<string, any>;
 
 export type MethodArgs = Array<any>;
 
-/**
- *
- */
 export type Request = {
     method: string;
     params?: Params | MethodArgs;
-}
+};
 
 export type Method = {
     namespace: string;
     name: string;
     params: Params;
     func: Function;
-}
+};
 
 export type HandlerResult = {
     error: boolean;
     data: any;
     func: Function;
     args: MethodArgs;
-}
+};
 
 export interface MessageHandler {
+    /**
+     * Should implement message parsing, request validation, method validation and param validation
+     *
+     * @param message {WebSocket.Data} - received message
+     * @param registeredMethods {Map<string, Method>} - methods registered in namespace
+     * @returns {HandlerResult} - result of message validation
+     */
     handle(message: WebSocket.Data, registeredMethods: Map<string, Method>): HandlerResult;
     process(handlerResult: HandlerResult): WebSocket.Data | undefined | Promise<WebSocket.Data | undefined>;
 }
