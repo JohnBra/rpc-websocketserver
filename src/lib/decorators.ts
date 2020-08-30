9043b806f1c005c6d1c48fc3c7ed4d62f4a4ade5
John <jonathan.braat@live.com>
2020-08-30 17:23:46 +0200

    [ADD] comments to decorators

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -1,8 +1,16 @@
 import 'reflect-metadata';
 import { Method, Params } from './interfaces';
 
+/**
+ * Symbol to be appended to function metadata
+ */
 export const PARAM_NAMES_KEY = Symbol('__paramNames__');
 
+/**
+ * Decorator factory to register a function to a namespace
+ *
+ * @param methodName {string} - optional parameter to rename the function name to the provided string
+ */
 export function register(methodName?: string): Function {
     return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor): void {
         const paramNames: Array<string> = Reflect.getOwnMetadata(PARAM_NAMES_KEY, target, propertyKey) || [];
@@ -28,9 +36,14 @@ export function register(methodName?: string): Function {
     };
 }
 
-// TODO add limitations of parameters to readme (no type checking implemented)
+/**
+ * Decorator factory to expose function parameter to registered method
+ *
+ * @param name {string} - associated name for param index
+ */
 export function param(name: string): Function {
     return function (target: Object, propertyKey: string | symbol, parameterIndex: number): void {
+        // TODO add limitations of parameters to readme (no type checking implemented)
         const paramNames = Reflect.getOwnMetadata(PARAM_NAMES_KEY, target, propertyKey) || [];
 
         paramNames[parameterIndex] = name;
